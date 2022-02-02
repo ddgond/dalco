0b52a100cb7b9fe040eb7a38117c7c7197c72a7c
Daniel Dangond <dadangond@protonmail.com>
2022-02-02 15:22:38 -0500

    add -v to list of valid flags for version

--- bin/dalco.js
+++ bin/dalco.js
@@ -9,7 +9,7 @@ const program = new Command();
 program
   .name('dalco')
   .description('A swiss army knife of tools.')
-  .version('1.1.2');
+  .version('1.1.3', '-v, -V, --version');
   
 compress(program);
 update(program);
